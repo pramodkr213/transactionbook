5ad9bbba25789595382b345e743e3dc9f614a645
pramod <iamppramodkr@gmail.com>
2025-04-07 16:45:35 +0530

    push

--- src/main/java/com/transaction/book/config/SecurityConfig.java
+++ src/main/java/com/transaction/book/config/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import com.google.api.client.util.Value;
+import org.springframework.beans.factory.annotation.Value;
 import com.transaction.book.jwtSecurity.CustomUserDetail;
 import com.transaction.book.jwtSecurity.JwtValidator;
 import com.transaction.book.services.serviceImpl.JwtTokenServiceImpl;
